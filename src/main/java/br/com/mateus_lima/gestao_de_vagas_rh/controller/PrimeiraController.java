7ddb1c6331aa84c85515e9ebcc292055bc2403af
Mateus-R-De-Lima <limamateus67@gmail.com>
2025-04-22 22:24:15 -0300

    Refatoração realizada com foco no entendimento e prática das anotações @PathVariable e @RequestParam no Spring Boot.
    @PathVariable: permite extrair valores diretamente da URL (ex: /usuarios/{id}) para uso nos métodos do controller.
    
    @RequestParam: permite capturar parâmetros de consulta (query string) como em /usuarios?nome=João.

--- src/main/java/br/com/mateus_lima/gestao_de_vagas_rh/controller/PrimeiraController.java
+++ src/main/java/br/com/mateus_lima/gestao_de_vagas_rh/controller/PrimeiraController.java
@@ -1,15 +1,25 @@
 package br.com.mateus_lima.gestao_de_vagas_rh.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController //utilizamos a annotation @RestController para indicar que a classe é um Controller e que estamos construindo uma API REST.
 @RequestMapping("/primeiraController") //utilizamos a annotation @RequestMapping para definir o recurso da rota
 public class PrimeiraController {
 
-    @GetMapping("/primeiroMetodo")
-    public String primeiroMetodo(){
-        return "Meu primeiro endpoit";
+    @GetMapping("/primeiroMetodo/{id}")
+    public String primeiroMetodo(@PathVariable String id){
+        return "O paramentro é " + id;
+    }
+
+    @GetMapping("/metodoComQueryParams")
+
+    public  String metodoComQuaryParamas(@RequestParam String id){
+        return "O paramétro com metodoComQueryParams é " +id;
+    }
+    @GetMapping("/metodoComQueryParams2")
+    public  String metodoComQuaryParamas2(@RequestParam Map<String,String> allParams){
+        return "O paramétro com metodoComQueryParams 2 é " + allParams.entrySet();
     }
 }
