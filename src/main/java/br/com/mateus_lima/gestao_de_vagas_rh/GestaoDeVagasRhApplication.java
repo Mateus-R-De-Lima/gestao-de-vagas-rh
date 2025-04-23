ed8dae0b992da11fe2c5eaffd36210e1fd55d4eb
Mateus-R-De-Lima <limamateus67@gmail.com>
2025-04-22 21:52:56 -0300

    Projeto inicial.

---
+++ src/main/java/br/com/mateus_lima/gestao_de_vagas_rh/GestaoDeVagasRhApplication.java
@@ -0,0 +1,13 @@
+package br.com.mateus_lima.gestao_de_vagas_rh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoDeVagasRhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoDeVagasRhApplication.class, args);
+	}
+
+}
