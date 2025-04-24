087191929167e344218968e69018a5bb64831498
Mateus-R-De-Lima <limamateus67@gmail.com>
2025-04-23 21:48:35 -0300

    Adiciona nova funcionalidade com validações utilizando anotações do Spring Boot para garantir integridade dos dados recebidos

--- src/main/java/br/com/mateus_lima/gestao_de_vagas_rh/primeiroprojetospringboot/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/mateus_lima/gestao_de_vagas_rh/primeiroprojetospringboot/modules/candidate/CandidateEntity.java
@@ -1,6 +1,9 @@
 package br.com.mateus_lima.gestao_de_vagas_rh.primeiroprojetospringboot.modules.candidate;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 import java.util.UUID;
 
@@ -9,8 +12,11 @@ public class CandidateEntity {
 
     private  UUID id;
     private  String name;
+    @Pattern(regexp = "^(?!\\s*$).+", message = "O campo username não deve conter espaços")
     private  String username;
+    @Email(message = "Não foi possível validar o e-mail. Verifique se está completo e corretamente digitado.")
     private  String email;
+    @Length(min = 6, max = 50, message = "Por favor, insira uma senha válida (entre 6 e 50 caracteres)")
     private  String password;
     private  String description;
     private  String curriculum;
