b4d20dad82d3510f3bf800001683cc998dfb3b98
Mateus-R-De-Lima <limamateus67@gmail.com>
2025-05-14 20:48:06 -0300

    feat(security): adiciona suporte a JWT no Swagger com @SecurityScheme e @SecurityRequirement refactor(repository): ajusta consulta para ignorar case sensitivity
    - Anotação @SecurityScheme adicionada para definir o esquema de autenticação JWT na documentação Swagger
    - @SecurityRequirement aplicado aos endpoints protegidos
    - Modificado método no repositório para realizar buscas case-insensitive (ex: usando LOWER ou ILIKE)

--- src/main/java/br/com/mateus_lima/gestao_de_vagas_rh/primeiroprojetospringboot/GestaoDeVagasRhApplication.java
+++ src/main/java/br/com/mateus_lima/gestao_de_vagas_rh/primeiroprojetospringboot/GestaoDeVagasRhApplication.java
@@ -1,7 +1,10 @@
 package br.com.mateus_lima.gestao_de_vagas_rh.primeiroprojetospringboot;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -9,15 +12,17 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackages = "br.com.mateus_lima.gestao_de_vagas_rh")
 @OpenAPIDefinition(
-		info = @Info(
-				title = "Gestão de Vagas",
-				description = "API responsável pela gestão de vagas"
-		)
+        info = @Info(
+                title = "Gestão de Vagas",
+                description = "API responsável pela gestão de vagas",
+                version = "1"
+        )
 )
+@SecurityScheme(name = "jwt_auth", scheme = "bearer", bearerFormat = "JWT", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 public class GestaoDeVagasRhApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GestaoDeVagasRhApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GestaoDeVagasRhApplication.class, args);
+    }
 
 }
