0cb23239e8581bb931350b0d2f09999b5a9f4c1b
Mateus-R-De-Lima <limamateus67@gmail.com>
2025-05-14 21:00:57 -0300

    feat(docs): adiciona anotações @Schema para documentação.
    - Utilização da anotação @Schema nos DTOs e modelos de domínio
    - Define descrições, exemplos e constraints para os campos
    - Melhora a geração automática da documentação Swagger/OpenAPI

--- src/main/java/br/com/mateus_lima/gestao_de_vagas_rh/primeiroprojetospringboot/modules/company/controllers/JobController.java
+++ src/main/java/br/com/mateus_lima/gestao_de_vagas_rh/primeiroprojetospringboot/modules/company/controllers/JobController.java
@@ -3,6 +3,14 @@ package br.com.mateus_lima.gestao_de_vagas_rh.primeiroprojetospringboot.modules.
 import br.com.mateus_lima.gestao_de_vagas_rh.primeiroprojetospringboot.modules.company.dto.JobDTO;
 import br.com.mateus_lima.gestao_de_vagas_rh.primeiroprojetospringboot.modules.company.entities.JobEntity;
 import br.com.mateus_lima.gestao_de_vagas_rh.primeiroprojetospringboot.modules.company.useCases.CreateJobUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +32,18 @@ public class JobController {
     private CreateJobUseCase createJobUseCase;
     @PostMapping("/")
     @PreAuthorize("hasRole('company')")
+    @Tag(name = "Vaga",description = "Descrição da Vaga")
+    @Operation(summary = "Cadastro de vagas", description = "Essa função é responsável por cadastrar as vagas dentro da empresa")
+    @ApiResponses(
+            {
+                    @ApiResponse(responseCode = "200",content = {
+                            @Content(array = @ArraySchema(
+                                    schema = @Schema(implementation = JobEntity.class)
+                            ))
+                    })
+            }
+    )
+    @SecurityRequirement(name = "jwt_auth")
     public ResponseEntity<Object> create(@Valid @RequestBody JobDTO jobDTO, HttpServletRequest request) {
         try {
             // Recuperar o companyId
--- src/main/java/br/com/mateus_lima/gestao_de_vagas_rh/primeiroprojetospringboot/modules/company/dto/JobDTO.java
+++ src/main/java/br/com/mateus_lima/gestao_de_vagas_rh/primeiroprojetospringboot/modules/company/dto/JobDTO.java
@@ -1,10 +1,14 @@
 package br.com.mateus_lima.gestao_de_vagas_rh.primeiroprojetospringboot.modules.company.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class JobDTO {
+    @Schema(example = "Plano de saúde, vale-refeição, bônus anual")
     private  String benifits;
+    @Schema(example = "JAVA Spring Boot")
     private String description;
+    @Schema(example = "JUNIOR")
     private  String level;
 }
